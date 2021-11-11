17626adb78c24c8d7204e4dc6618edc3b07acaad
katakabe <shohei.katakabe@eeeeg.tokyo>
2021-11-11 18:46:13 +0900

    add:Added ObjectiveFactory.php

---
+++ database/factories/ObjectiveFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Okr;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ObjectiveFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'result'    => $this->faker->realText(50),
+            'score'     => $this->faker->randomFloat(1, 0, 1), //小数点第1までの0から1のランダムな浮動小数点
+            'okr_id'    => Okr::factory(),
+        ];
+    }
+}
