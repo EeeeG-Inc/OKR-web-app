5e7ed8719ea8dcd7fb186b6a7512361c34540f59
katakabe <shohei.katakabe@eeeeg.tokyo>
2021-11-11 18:17:04 +0900

    add:Added CompanyFactory.php

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\CompanyGroup;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name'              => $this->faker->company(),
+            'company_group_id'  => CompanyGroup::factory(),
+            'is_master'         => $this->faker->boolean(50),
+        ];
+    }
+}
