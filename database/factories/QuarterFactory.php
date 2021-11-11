424b413995fac4cb4c61c81dfd3ebaaaa1d6dc74
katakabe <shohei.katakabe@eeeeg.tokyo>
2021-11-11 18:45:14 +0900

    add:Added QuarterFactory.php

---
+++ database/factories/QuarterFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QuarterFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'from'          => $this->faker->month(),
+            'to'            => $this->faker->month(),
+            'company_id'    => Company::factory(),
+        ];
+    }
+}
