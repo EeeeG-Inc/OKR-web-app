5bf7a07b2ebbb11359d260b9c0c4618bd055aabe
katakabe <shohei.katakabe@eeeeg.tokyo>
2021-12-02 18:58:58 +0900

    add:Added lang files

--- resources/lang/ja/common/title.php
+++ resources/lang/ja/common/title.php
@@ -2,6 +2,11 @@
 
 return [
     'okr' => [
-        'index' => 'OKR 一覧'
+        'index' => 'OKR 一覧',
+        'create' => 'OKR 登録',
+        'type' => '登録区分',
+    ],
+    'objective' => [
+        'forOkr' => '成果指標に紐づく目標',
     ],
 ];
---
+++ resources/lang/ja/models/quarters.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'fields' => [
+        'id' => 'ID',
+        "quater" => "四半期区分",
+        "from" => "開始月",
+        "to" => "終了月",
+        "company_id" => "会社ID",
+    ],
+    'quarter' => [
+        'firstQuarter' => '第1四半期',
+        'secondQuarter' => '第2四半期',
+        'thirdQuarter' => '第3四半期',
+        'fourthQuarter' => '第4四半期',
+    ],
+];
