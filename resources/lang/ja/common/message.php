31e85f146584c6c2159472ec8434917739173b38
Takuma-Ikeda <takuma.ikeda@eeeeg.tokyo>
2021-12-23 19:33:26 +0900

    feat: OKR 更新機能

--- resources/lang/ja/common/message.php
+++ resources/lang/ja/common/message.php
@@ -3,6 +3,7 @@
 return [
     'objective' => [
         'store' => 'OKR の登録が完了しました',
+        'update' => 'OKR の更新が完了しました',
         'delete_confirm' => '「:objective」および「該当する成果指標」を削除してもよろしいでしょうか？',
         'delete_failed' => '「:objective」および「該当する成果指標」の削除に失敗しました',
         'delete_success' => '「:objective」および「該当する成果指標」の削除に成功しました',
