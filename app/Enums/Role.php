82caaa09214a162ba9953f709ced67885f6e569f
katakabe <shohei.katakabe@eeeeg.tokyo>
2021-12-23 18:55:39 +0900

    fix:下位 Role 作成のメソッド追加

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -4,6 +4,7 @@ namespace App\Enums;
 
 use BenSampo\Enum\Contracts\LocalizedEnum;
 use BenSampo\Enum\Enum;
+use Ramsey\Uuid\Type\Integer;
 
 /**
  * @method static static OptionOne()
@@ -17,4 +18,18 @@ class Role extends Enum implements LocalizedEnum
     const DEPARTMENT = 3;
     const MANAGER = 4;
     const MEMBER = 5;
+
+    public static function getRolesInWhenCreateUser(int $myRoleVal): array
+    {
+        $results = [];
+        foreach (self::getValues() as $value) {
+            if (($myRoleVal === self::COMPANY) && ($value === self::COMPANY)) {
+                $results[$value] = self::getLocalizedDescription($value);
+            }
+            if ($value > $myRoleVal) {
+                $results[$value] = self::getLocalizedDescription($value);
+            }
+        }
+        return $results;
+    }
 }
---
+++ resources/lang/ja/enums.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Enums\Role;
+
+return [
+    Role::class => [
+        Role::ADMIN         => '管理者',
+        Role::COMPANY       => '会社',
+        Role::DEPARTMENT    => '部署',
+        Role::MANAGER       => 'マネージャー',
+        Role::MEMBER        => '一般',
+    ],
+];
