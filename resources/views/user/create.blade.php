ce4367d1a0cfff79ea17b3a0016fe8cb859da797
katakabe <shohei.katakabe@eeeeg.tokyo>
2021-12-27 17:59:45 +0900

    fix:create function 修正

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -5,14 +5,13 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ '新規ユーザ作成' }}</div>
+                    <div class="card-header">{{ __('common/title.user.create') }}</div>
                     <div class="card-body">
                         <div class="pt-4 bg-gray-100">
                             <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
                                 <div
                                     class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
                                     <div class="form">
-                                        {{-- TODO:全体のタイトル変更と多言語管理 --}}
                                         {{ Form::open(['url' => route('user.store'), 'files' => true]) }}
                                         {{-- CSRFトークン --}}
                                         {{ Form::token() }}
@@ -21,37 +20,37 @@
                                         {{-- 氏名 --}}
                                         <div class="form-group row">
                                             <div class="col-md-2 mb-3">
-                                                {{ Form::label('inputName', '氏名') }}
+                                                {{ Form::label('name', __('models/users.fields.name')) }}
                                             </div>
                                             <div class="col-md-10">
-                                                {{ Form::text('inputName', null, ['class' => 'form-control', 'id' => 'inputName']) }}
+                                                {{ Form::text('name', null, ['class' => 'form-control', 'id' => 'name']) }}
                                             </div>
                                         </div>
 
                                         {{-- Role --}}
                                         <div class="form-group row">
                                             <div class="col-md-2 mb-3">
-                                                {{ Form::label('selectRoles', '権限') }}
+                                                {{ Form::label('role', __('models/users.fields.role')) }}
                                             </div>
                                             <div class="col-md-10">
-                                                {{ Form::select('roles', $roles, null, ['class' => 'form-control', 'id' => 'roles']) }}
+                                                {{ Form::select('role', $roles, null, ['class' => 'form-control', 'id' => 'role']) }}
                                             </div>
                                         </div>
 
                                         {{-- 部署 --}}
                                         <div class="form-group row">
                                             <div class="col-md-2 mb-3">
-                                                {{ Form::label('selectDepartments', '部署') }}
+                                                {{ Form::label('departments', __('models/departments.fields.name')) }}
                                             </div>
                                             <div class="col-md-10">
-                                                {{ Form::select('departments', $departmentNames, null, ['class' => 'form-control', 'id' => 'departments']) }}
+                                                {{ Form::select('departments', ['' => '-']+$departmentNames, null, ['class' => 'form-control', 'id' => 'departments']) }}
                                             </div>
                                         </div>
 
                                         {{-- Email --}}
                                         <div class="form-group row">
                                             <div class="col-md-2 mb-3">
-                                                {{ Form::label('email', 'Eメール') }}
+                                                {{ Form::label('email', __('models/users.fields.email')) }}
                                             </div>
                                             <div class="col-md-10">
                                                 {{ Form::email('email', null, ['class' => 'form-control', 'id' => 'email']) }}
@@ -61,7 +60,7 @@
                                         {{-- パスワード --}}
                                         <div class="form-group row">
                                             <div class="col-md-2 mb-3">
-                                                {{ Form::label('password', 'パスワード') }}
+                                                {{ Form::label('password', __('models/users.fields.password')) }}
                                             </div>
                                             <div class="col-md-10 mb-3">
                                                 {{ Form::password('password', ['class' => 'form-control', 'id' => 'password']) }}
