82e6e4479176f2b545eeb3fd5017e3e7298d8afd
Takuma-Ikeda <takuma.ikeda@eeeeg.tokyo>
2022-02-07 10:32:41 +0900

    fix: 新規ユーザ作成 / Slack メニューに権限判定をつける。ログイン前は nav メニュー表示しない

--- resources/lang/ja/common/action.php
+++ resources/lang/ja/common/action.php
@@ -12,7 +12,7 @@ return [
     'stop' => '停止',
     'restart' => '有効化',
     'create' => '新規作成',
-    'create_company' => '法人アカウント作成',
+    'create_company' => '会社アカウント作成',
     'confirm' => '内容確認',
     'update' => '更新',
 ];
