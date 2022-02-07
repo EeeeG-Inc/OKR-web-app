6e3d71fe91012d520a9bc110545a2d4577e43494
Takuma-Ikeda <takuma.ikeda@eeeeg.tokyo>
2022-02-07 11:42:26 +0900

    fix: CircleCI 設定修正

--- resources/views/quarter/edit.blade.php
+++ resources/views/quarter/edit.blade.php
@@ -8,7 +8,7 @@
                 <div class="card">
                     <div class="card-header">{{ __('common/title.quarter.edit') }}</div>
                     <div class="card-body">
-                        <div class="pt-4 bg-gray-100">
+                        <div class="bg-gray-100">
                             <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
                                 <div
                                     class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
