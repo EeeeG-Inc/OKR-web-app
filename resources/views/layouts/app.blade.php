4d78f1af62293164d3d2b05f66e12a3898d6567a
Takuma-Ikeda <takuma.ikeda@eeeeg.tokyo>
2022-02-08 11:49:38 +0900

    fix: title を表示する / script は body の後で読み込むようにする

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,10 +10,6 @@
 
     <title>@yield('title')</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -113,5 +109,10 @@
         </main>
     </div>
 </body>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+@stack('scripts')
 
 </html>
--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title', __('common/title.user.create'))
 
 @section('content')
     <div class="container-fluid">
@@ -98,101 +99,107 @@
             </div>
         </div>
     </div>
-    <script>
-        $(function() {
-            const COMPANY = 2;
-            const DEPARTMENT = 3;
-            const MANAGER = 4;
-            const MEMBER = 5;
-            actions = {
-                COMPANY: '{{ route('company.store') }}',
-                DEPARTMENT: '{{ route('department.store') }}',
-                MANAGER: '{{ route('manager.store') }}',
-                MEMBER: '{{ route('member.store') }}',
-            };
-            roleId = 0;
 
-            var controlFields = function (roleId) {
-                switch (roleId) {
-                    case COMPANY:
-                        $('#name').text('{!! __('common/label.user.create.name_company')!!}');
-                        $('#create_user_form').attr('action', actions.COMPANY);
-                        $('#company_form').hide();
-                        $('#department_form').hide();
-                        break;
-                    case DEPARTMENT:
-                        $('#name').text('{!! __('common/label.user.create.name_department')!!}');
-                        $('#create_user_form').attr('action', actions.DEPARTMENT);
-                        $('#company_form').show();
-                        $('#department_form').hide();
-                        break;
-                    case MANAGER:
-                        $('#name').text('{!! __('common/label.user.create.name_manager')!!}');
-                        $('#create_user_form').attr('action', actions.MANAGER);
-                        $('#company_form').show();
-                        $('#department_form').show();
-                        break;
-                    case MEMBER:
-                        $('#name').text('{!! __('common/label.user.create.name_member')!!}');
-                        $('#create_user_form').attr('action', actions.MEMBER);
-                        $('#company_form').show();
-                        $('#department_form').show();
-                        break;
-                }
-            }
+    @push('scripts')
+        <script>
+            $(function() {
+                const COMPANY = 2;
+                const DEPARTMENT = 3;
+                const MANAGER = 4;
+                const MEMBER = 5;
+                actions = {
+                    COMPANY: '{{ route('company.store') }}',
+                    DEPARTMENT: '{{ route('department.store') }}',
+                    MANAGER: '{{ route('manager.store') }}',
+                    MEMBER: '{{ route('member.store') }}',
+                };
+                roleId = 0;
 
-            var postData = function (url = '', data = {}) {
-                return fetch(url, {
-                    method: "POST",
-                    mode: 'same-origin',
-                    credentials: 'include',
-                    headers: {
-                        "Content-Type": "application/json; charset=utf-8",
-                        "X-CSRF-TOKEN": "{{ csrf_token() }}"
-                    },
-                    body: JSON.stringify(data)
-                })
-                .then(response => response.json());
-            }
-
-            var fetchDepartments = function (companyId) {
-                postData("{{ route('fetch.departments', $companyId) }}", {companyId: companyId})
-                .then(data => {
-                    $('#departments').children().remove();
-                    if (data.departments.length === 0) {
-                        $('#departments').append($('<option>').attr({ value: '' }).text('部署ユーザを作成してください'));
-                        $('#departments').prop('disabled', true);
-                    } else {
-                        $('#departments').append($('<option>').attr({ value: '' }).text('部署名を選択してください'));
-                        $.each(data.departments, function(index, value) {
-                            $('#departments').append($('<option>').attr({ value: value.id }).text(value.name));
-                            $('#departments').prop('disabled', false);
-                        });
+                var controlFields = function (roleId) {
+                    switch (roleId) {
+                        case COMPANY:
+                            $('#name').text('{!! __('common/label.user.create.name_company')!!}');
+                            $('#create_user_form').attr('action', actions.COMPANY);
+                            $('#company_form').hide();
+                            $('#department_form').hide();
+                            break;
+                        case DEPARTMENT:
+                            $('#name').text('{!! __('common/label.user.create.name_department')!!}');
+                            $('#create_user_form').attr('action', actions.DEPARTMENT);
+                            $('#company_form').show();
+                            $('#department_form').hide();
+                            break;
+                        case MANAGER:
+                            $('#name').text('{!! __('common/label.user.create.name_manager')!!}');
+                            $('#create_user_form').attr('action', actions.MANAGER);
+                            $('#company_form').show();
+                            $('#department_form').show();
+                            break;
+                        case MEMBER:
+                            $('#name').text('{!! __('common/label.user.create.name_member')!!}');
+                            $('#create_user_form').attr('action', actions.MEMBER);
+                            $('#company_form').show();
+                            $('#department_form').show();
+                            break;
                     }
-                })
-                .catch(error => {
-                    console.error(error.message)
+                }
+
+                var postData = function (url = '', data = {}) {
+                    return fetch(url, {
+                        method: "POST",
+                        mode: 'same-origin',
+                        credentials: 'include',
+                        headers: {
+                            "Content-Type": "application/json; charset=utf-8",
+                            "X-CSRF-TOKEN": "{{ csrf_token() }}"
+                        },
+                        body: JSON.stringify(data)
+                    })
+                    .then(response => response.json());
+                }
+
+                var fetchDepartments = function (companyId) {
+                    postData("{{ route('fetch.departments', $companyId) }}", {companyId: companyId})
+                    .then(data => {
+                        $('#departments').children().remove();
+                        if (data.departments.length === 0) {
+                            $('#departments').append($('<option>').attr({ value: '' }).text('部署ユーザを作成してください'));
+                            $('#departments').prop('disabled', true);
+                        } else {
+                            $('#departments').append($('<option>').attr({ value: '' }).text('部署名を選択してください'));
+                            $.each(data.departments, function(index, value) {
+                                $('#departments').append($('<option>').attr({ value: value.id }).text(value.name));
+                                $('#departments').prop('disabled', false);
+                            });
+                        }
+                    })
+                    .catch(error => {
+                        // console.error(error.message)
+                        $('#departments').children().remove();
+                        $('#departments').append($('<option>').attr({ value: '' }).text('部署データを取得できませんでした'));
+                        $('#departments').prop('disabled', true);
+                    });
+                }
+
+                $('#roles').change(function() {
+                    roleId = Number($('#roles').val())
+                    controlFields(roleId)
+                    if ((roleId === MANAGER) || (roleId === MEMBER)) {
+                        fetchDepartments(Number($('#companies').val()))
+                    }
                 });
-            }
 
-            $('#roles').change(function() {
-                roleId = Number($('#roles').val())
-                controlFields(roleId)
-                if ((roleId === MANAGER) || (roleId === MEMBER)) {
-                    fetchDepartments(Number($('#companies').val()))
-                }
-            });
+                $('#companies').change(function() {
+                    roleId = Number($('#roles').val())
+                    if ((roleId === MANAGER) || (roleId === MEMBER)) {
+                        fetchDepartments(Number($('#companies').val()))
+                    }
+                });
 
-            $('#company_form').change(function() {
-                roleId = Number($('#roles').val())
-                if ((roleId === MANAGER) || (roleId === MEMBER)) {
-                    fetchDepartments(Number($('#companies').val()))
-                }
-            });
-
-            // 初期化
-            controlFields(Number($('#roles').val()));
-            fetchDepartments(Number($('#companies').val()))
-        })
-    </script>
+                // 初期化
+                controlFields(Number($('#roles').val()));
+                fetchDepartments(Number($('#companies').val()))
+            })
+        </script>
+    @endpush
 @endsection
