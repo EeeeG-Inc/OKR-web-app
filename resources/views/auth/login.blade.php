6dd1507736c7e23ca5175362b7a07a4ec439f629
Takuma-Ikeda <takuma.ikeda@eeeeg.tokyo>
2022-02-08 18:21:18 +0900

    fix: auth 系の Blade に title 設定

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title', __('common/title.auth.login'))
 
 @section('content')
 <div class="container-fluid">
--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title', __('common/title.auth.confirm'))
 
 @section('content')
 <div class="container-fluid">
