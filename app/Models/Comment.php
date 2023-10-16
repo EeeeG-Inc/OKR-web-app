bd014905fb43cf1895714aa69ecbe1e5a63f60ae
ShutaTanaka <shuta.tanaka@eeeeg.tokyo>
2023-10-16 18:05:55 +0900

    fix:テキストに URL が含まれていたらリンク化してほしい
    本日作業分
    コメントがURL化したことは確認できた。
    コメントに複数URLがあった場合などは未確認。

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -81,5 +81,37 @@ class Comment extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    /**
+     * CommentにURLが含まれていた場合、リンク化する。
+     *
+     * @param [type] $value
+     * @return void
+     */
+    public function getCommentAttribute($value)
+    {
+        //PHP_EOLは,改行コードをあらわす.改行があれば分割する
+        $texts = explode(PHP_EOL, $value);
+        //正規表現パターン
+        $pattern = '/^https?:\/\/[^\s 　\\\|`^"\'(){}<>\[\]]*$/';
+        //空の配列を用意
+        $replacedTexts = array();
+
+        foreach ($texts as $value) {
+            $replace = preg_replace_callback($pattern, function ($matches) {
+                //textが１行ごとに正規表現にmatchするか確認する
+                if (isset($matches[1])) {
+                    return $matches[0]; //$matches[0] がマッチした全体を表す
+                }
+                //既にリンク化してあれば置換は必要ないので、配列に代入
+                return '<a href="' . $matches[0] . '" target="_blank" rel="noopener">' . $matches[0] . '</a>';
+            }, $value);
+            //リンク化したコードを配列に代入
+            $replacedTexts[] = $replace;
+        }
+        //配列にしたtextを文字列にする
+        return implode(PHP_EOL, $replacedTexts);
+
+    }
 }
 
