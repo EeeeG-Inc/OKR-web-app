688eccebe8faf17e70c0e2ef1c6a9e72ad8626be
ShutaTanaka <shuta.tanaka@eeeeg.tokyo>
2023-10-20 16:36:19 +0900

    fix:指摘の修正
    修正しマイグレーションが実行できることが確認できた。

---
+++ database/migrations/2023_10_20_161214_create_comment_like_users_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentLikeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment_like_users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('comment_id')
+                ->unsigned()
+                ->comment('コメントID');
+            $table->integer('user_id')
+                ->unsigned()
+                ->comment('ユーザID');
+            $table->boolean('is_like')
+                ->default(false)
+                ->comment('いいね');
+            $table->timestamps();
+
+            $table->foreign('comment_id')
+            ->references('id')
+            ->on('comments');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comment_like_users', function (Blueprint $table): void {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['comment_id']);
+        });
+        Schema::dropIfExists('comment_like_users');
+    }
+
+}
