1772bac3865d555d126d34b60ade5568a579f526
ShutaTanaka <shuta.tanaka@eeeeg.tokyo>
2024-02-20 17:41:44 +0900

    fix:stackとpushを使って、cdnをapp.blade.phpのheadに入れる。 コメントアウトしていた、jQueryを削除。

--- resources/views/key_result/index.blade.php
+++ resources/views/key_result/index.blade.php
@@ -183,7 +183,7 @@
             </div>
         </div>
     </div>
-    @push('jquery')
+    @push('scripts')
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     @endpush
     <script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <link href="{{ asset('css/common.css') }}" rel="stylesheet">
     <link href="https://use.fontawesome.com/releases/v6.0.0/css/all.css" rel="stylesheet">
 
-    @stack('jquery')
+    @stack('scripts')
 </head>
 
 <body>
@@ -148,7 +148,6 @@
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}" defer></script>
-{{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> --}}
 @stack('scripts')
 
 </html>
